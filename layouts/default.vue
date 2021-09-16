51e74a93b46e5594b38f608b1bca76fc8d27dcd0
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-09-16 23:07:18 +0700

    [Add] layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center">
-    <nuxt />
+  <div class="min-h-screen grid grid-cols-6 justify-center">
+    <div class="border-r border-gray-400">Sidebar</div>
+    <nuxt class="col-span-4" />
+    <div class="border-l border-gray-400">Sidebar</div>
   </div>
 </template>
