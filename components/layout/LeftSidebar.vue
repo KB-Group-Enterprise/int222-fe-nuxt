7b010226065ba046900d14cc92c400c35f5d272d
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-09-16 23:23:45 +0700

    [Add] Left Sidebar

---
+++ components/layout/LeftSidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-3">
+    <img
+      class="w-2/3 flex justify-center mx-auto"
+      :src="require(`@/assets/images/icon/Logo.png`)"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, useContext } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style></style>
