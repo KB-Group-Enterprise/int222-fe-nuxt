b0ee70bc91af3dceced3d88a3fbdfceb2330ea5a
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-09-17 00:57:48 +0700

    [Update] Left Sidebar Items

--- components/layout/LeftSidebar.vue
+++ components/layout/LeftSidebar.vue
@@ -4,6 +4,30 @@
       class="w-2/3 flex justify-center mx-auto"
       :src="require(`@/assets/images/icon/Logo.png`)"
     />
+    <div class="flex flex-col mt-10 space-y-4">
+      <div
+        v-for="item in items"
+        :key="item.name"
+        class="flex rounded-lg p-2 mx-2 cursor-pointer"
+        :class="selectedItem === item.name ? 'bg-gray-800' : ''"
+        @click="selectItem(item.name)"
+      >
+        <div
+          class="
+            btn btn-circle
+            border-none
+          "
+          :class="
+            selectedItem === item.name
+              ? 'bg-gradient-to-r from-blue-500 to-indigo-600'
+              : ''
+          "
+        >
+          <font-awesome-icon :icon="item.icon" />
+        </div>
+        <div class="pl-4 flex items-center text-lg">{{ item.name }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +36,19 @@ import { defineComponent, ref, useContext } from '@nuxtjs/composition-api';
 
 export default defineComponent({
   setup() {
-    return {};
+    const items = [
+      { name: 'Dashboard', icon: 'home' },
+      { name: 'Prakit', icon: 'home' },
+      { name: 'Ruangrit', icon: 'home' },
+      { name: 'Sapondanai', icon: 'home' },
+    ];
+    const selectedItem = ref('Dashboard');
+
+    const selectItem = (item) => {
+      selectedItem.value = item;
+    };
+
+    return { items, selectItem, selectedItem };
   },
 });
 </script>
