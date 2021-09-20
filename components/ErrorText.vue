dabff56582c3e1cdf7acd8835e7a3c2d44212269
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-09-20 16:09:16 +0700

    [Update] sign up valiation

---
+++ components/ErrorText.vue
@@ -0,0 +1,16 @@
+<template>
+  <span v-if="error" class="text-red-500 pt-5">{{ error }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style></style>
--- components/auth/Signup.vue
+++ components/auth/Signup.vue
@@ -1,61 +1,99 @@
 <template>
-  <form class="form-control w-full py-4" @submit.prevent="signup">
-    <label class="label" for="username">Username</label>
-    <input
-      id="username"
-      v-model="userData.username"
-      class="input input-bordered"
-      type="text"
-    />
-    <label class="label" for="password">Password</label>
-    <input
-      id="password"
-      v-model="userData.password"
-      class="input input-bordered"
-      type="password"
-    />
-    <label class="label" for="confirmpassword">Confirm Password</label>
-    <input
-      id="confirmpassword"
-      v-model="userData.confirmPassword"
-      class="input input-bordered"
-      type="password"
-    />
-    <label class="label" for="question">Question</label>
-    <div v-if="!loading && result">
-      <select
-        id="question"
-        v-model="userData.questionId"
-        name="questions"
-        class="select select-bordered w-full"
-      >
-        <option disabled="disabled" selected="selected">
-          Choose your question for reset password
-        </option>
-        <option
-          v-for="question in result.questions"
-          :key="question.questionId"
-          :value="question.questionId"
-        >
-          {{ question.question }}
-        </option>
-      </select>
-    </div>
-    <label class="label" for="restoreanswer">Answer</label>
-    <input
-      id="answer"
-      v-model="userData.restoreAnswer"
-      class="input input-bordered"
-      type="text"
-    />
-    <button
-      type="submit"
-      class="btn btn-primary text-xs mt-5 md:text-base"
-      :class="[isSignUpLoading ? 'loading' : '']"
+  <ValidationObserver v-slot="{ handleSubmit }" class="w-full">
+    <form
+      class="form-control w-full py-4"
+      @submit.prevent="handleSubmit(signup)"
     >
-      {{ isSignUpLoading ? 'Loading' : 'Register' }}
-    </button>
-  </form>
+      <label class="label" for="username">Username</label>
+      <ValidationProvider
+        v-slot="{ errors }"
+        name="Username"
+        rules="required|min:4"
+      >
+        <input
+          id="username"
+          v-model="userData.username"
+          class="input input-bordered w-full"
+          type="text"
+        />
+        <error-text :error="errorDetail"></error-text>
+        <error-text :error="errors[0]"></error-text>
+      </ValidationProvider>
+      <label class="label" for="password">Password</label>
+      <ValidationProvider
+        v-slot="{ errors }"
+        name="Password"
+        rules="required"
+        vid="confirmation"
+      >
+        <input
+          id="password"
+          v-model="userData.password"
+          class="input input-bordered w-full"
+          type="password"
+        />
+        <error-text :error="errors[0]"></error-text>
+      </ValidationProvider>
+      <label class="label" for="confirmpassword">Confirm Password</label>
+      <ValidationProvider
+        v-slot="{ errors }"
+        name="ConfirmPassword"
+        rules="required|confirmed:confirmation"
+      >
+        <input
+          id="confirmpassword"
+          v-model="userData.confirmPassword"
+          class="input input-bordered w-full"
+          type="password"
+        />
+        <error-text :error="errors[0]"></error-text>
+      </ValidationProvider>
+      <label class="label" for="question">Question</label>
+      <div v-if="!loading && result">
+        <ValidationProvider
+          v-slot="{ errors }"
+          name="Question"
+          rules="required"
+        >
+          <select
+            id="question"
+            v-model="userData.questionId"
+            name="questions"
+            class="select select-bordered w-full"
+          >
+            <option disabled="disabled" selected="selected">
+              Choose your question for reset password
+            </option>
+            <option
+              v-for="question in result.questions"
+              :key="question.questionId"
+              :value="question.questionId"
+            >
+              {{ question.question }}
+            </option>
+          </select>
+          <error-text :error="errors[0]"></error-text>
+        </ValidationProvider>
+      </div>
+      <label class="label" for="restoreanswer">Answer</label>
+      <ValidationProvider v-slot="{ errors }" name="Answer" rules="required">
+        <input
+          id="answer"
+          v-model="userData.restoreAnswer"
+          class="input input-bordered w-full"
+          type="text"
+        />
+        <error-text :error="errors[0]"></error-text>
+      </ValidationProvider>
+      <button
+        type="submit"
+        class="btn btn-primary text-xs mt-5 md:text-base"
+        :class="[isSignUpLoading ? 'loading' : '']"
+      >
+        {{ isSignUpLoading ? 'Loading' : 'Register' }}
+      </button>
+    </form>
+  </ValidationObserver>
 </template>
 
 <script lang="ts">
@@ -64,6 +102,7 @@ import {
   reactive,
   ref,
   useContext,
+  watch,
 } from '@nuxtjs/composition-api';
 import { useQuery } from '@vue/apollo-composable/dist';
 import Question from '@/graphql/queries/question.gql';
@@ -73,23 +112,26 @@ export default defineComponent({
     const { result, loading, onResult } = useQuery(Question);
     const isSignUpLoading = ref(false);
     const questions = ref<RestoreQuestion[]>([]);
+    const errorDetail = ref('');
     const { $axios } = useContext();
     const userData = reactive({
-      username: '',
-      password: '',
-      confirmPassword: '',
-      restoreAnswer: '',
-      questionId: '',
+      username: 'boat',
+      password: '12345678',
+      confirmPassword: '12345678',
+      restoreAnswer: 'black',
+      questionId: '1',
     });
     onResult(async ({ data }) => {
       questions.value = await data.questions;
     });
+    watch(userData, () => {
+      errorDetail.value = '';
+    });
     const signup = async () => {
       try {
-        const res = await $axios.post('/auth/register', userData);
-        console.log(res);
+        await $axios.post('/auth/register', userData);
       } catch (err) {
-        console.log(err);
+        if (err) errorDetail.value = 'username already exist';
       }
     };
     return {
@@ -98,6 +140,7 @@ export default defineComponent({
       isSignUpLoading,
       userData,
       signup,
+      errorDetail,
     };
   },
 });
