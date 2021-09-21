a0c02b7994d32c79d2812e1df5bd0318481c9d6e
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-09-21 22:36:18 +0700

    [Update] redirect to login, fetch reviews, login logout in sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="min-h-screen grid grid-cols-6 justify-center">
-    <div class="lg:flex hidden border-r border-gray-400"><layout-LeftSidebar/></div>
+    <div class="lg:flex hidden border-r border-gray-400">
+      <layout-LeftSidebar />
+    </div>
     <nuxt class="lg:col-span-4 col-span-6" />
-    <div class="lg:flex hidden border-l border-gray-400">Sidebar</div>
+    <div class="lg:flex border-l border-gray-400 flex flex-col">
+      <p>Sidebar</p>
+      <button v-if="$auth.user" class="btn btn-warning" @click="$auth.logout()">
+        Logout
+      </button>
+      <button v-else class="btn btn-secondary" @click="$router.push('/auth')">
+        Login
+      </button>
+    </div>
   </div>
 </template>
--- pages/game/_id.vue
+++ pages/game/_id.vue
@@ -1,8 +1,12 @@
 <template>
-  <div v-if="!loading && game">
+  <div v-if="game">
     <h1>Game Id: {{ game.gameId }}</h1>
     <h1>Game Name: {{ game.gameName }}</h1>
-    <ValidationObserver v-slot="{ handleSubmit }">
+    <ValidationObserver
+      v-if="$auth.user"
+      v-slot="{ handleSubmit }"
+      class="mt-10"
+    >
       <form @submit.prevent="handleSubmit(createReview)">
         <label class="label" for="ratings">Ratings</label>
         <ValidationProvider
@@ -38,6 +42,9 @@
         </button>
       </form>
     </ValidationObserver>
+    <div v-else class="flex flex-col mt-20">
+      <p>You need to login before comment</p>
+    </div>
     <GameComment
       v-for="comment in comments"
       :key="comment.reviewId"
@@ -51,6 +58,8 @@
 <script lang="ts">
 import {
   defineComponent,
+  onBeforeMount,
+  onMounted,
   reactive,
   ref,
   useContext,
@@ -59,38 +68,53 @@ import {
 } from '@nuxtjs/composition-api';
 import { useMutation, useQuery } from '@vue/apollo-composable/dist';
 import GameQuery from '@/graphql/queries/game.gql';
+import ReviewQuery from '@/graphql/queries/review.gql';
 import CreateReview from '@/graphql/mutations/createReview.gql';
-import { CreateReviewInput, Game, Review } from '~/types/types';
+import { CreateReviewInput, Game, Review, User } from '~/types/types';
 export default defineComponent({
   setup() {
     const router = useRouter();
-    const route = useRoute();
-    const paramId = ref<Number>(Number(route.value.params.id));
     const game = ref<Game | null>(null);
     const { $auth } = useContext();
     const reviewData = reactive<CreateReviewInput>({
       rating: 0,
       comment: '',
-      userId: $auth.user!.userId as string,
+      userId: '',
       gameId: 0,
-    });
-    const { onResult, loading } = useQuery(GameQuery, {
-      id: paramId.value,
     });
     const { mutate: sendReview, loading: isCreating } =
       useMutation(CreateReview);
     const comments = ref<Review[]>([]);
-
-    onResult((result) => {
-      if (result.error) {
-        router.push('/');
-      } else {
-        game.value = result.data.game;
-        reviewData.gameId = game.value!.gameId;
-      }
+    const fetchGame = (paramId: number) => {
+      const { onResult } = useQuery(GameQuery, {
+        id: paramId,
+      });
+      onResult((result) => {
+        if (result.error) {
+          router.push('/');
+        } else {
+          game.value = result.data.game;
+          reviewData.gameId = game.value!.gameId;
+        }
+      });
+    };
+    const fetchComments = (paramId: number) => {
+      const { onResult } = useQuery(ReviewQuery, { id: paramId });
+      onResult((result) => {
+        comments.value = result.data.reviewByGameId;
+      });
+    };
+    onBeforeMount(() => {
+      const route = useRoute();
+      const paramId = Number(route.value.params.id);
+      fetchGame(paramId);
+      fetchComments(paramId);
     });
+
     async function createReview() {
       try {
+        const user = $auth.user as User;
+        reviewData.userId = user.userId;
         reviewData.rating = Number(reviewData.rating);
         const res = await sendReview({ reviewData });
         if (res) {
@@ -104,9 +128,7 @@ export default defineComponent({
       }
     }
     return {
-      paramId,
       game,
-      loading,
       reviewData,
       isCreating,
       createReview,
