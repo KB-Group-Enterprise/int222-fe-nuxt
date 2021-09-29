9340527d85fdcbd10805a0cae48213ac4a466375
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-09-29 21:59:37 +0700

    [Update] Add Login and register button LeftSidebar

--- components/layout/LeftSidebar.vue
+++ components/layout/LeftSidebar.vue
@@ -1,31 +1,42 @@
 <template>
-  <div class="p-3">
-    <img
-      class="w-2/3 flex justify-center mx-auto"
-      :src="require(`@/assets/images/icon/Logo.png`)"
-    />
-    <div class="flex flex-col mt-10 space-y-4">
-      <div
-        v-for="item in items"
-        :key="item.name"
-        class="flex rounded-lg p-2 mx-2 cursor-pointer"
-        :class="selectedItem === item.name ? 'bg-gray-800' : ''"
-        @click="selectItem(item.name)"
-      >
+  <div class="p-3 w-full flex flex-col justify-between">
+    <div class="flex flex-col">
+      <nuxt-link to="/">
+        <img
+          class="w-2/3 flex justify-center mx-auto"
+          :src="require(`@/assets/images/icon/Logo.png`)"
+        />
+      </nuxt-link>
+      <div class="flex flex-col mt-10 space-y-4">
         <div
-          class="
-            btn btn-circle
-            border-none
-          "
-          :class="
-            selectedItem === item.name
-              ? 'bg-gradient-to-r from-blue-500 to-indigo-600'
-              : ''
-          "
+          v-for="item in items"
+          :key="item.name"
+          class="flex rounded-lg p-2 mx-2 cursor-pointer"
+          :class="selectedItem === item.name ? 'bg-gray-800' : ''"
+          @click="selectItem(item.name)"
         >
-          <font-awesome-icon :icon="item.icon" />
+          <div
+            class="btn btn-circle border-none"
+            :class="
+              selectedItem === item.name
+                ? 'bg-gradient-to-r from-blue-500 to-indigo-600'
+                : ''
+            "
+          >
+            <font-awesome-icon :icon="item.icon" />
+          </div>
+          <div class="pl-4 flex items-center text-lg">{{ item.name }}</div>
         </div>
-        <div class="pl-4 flex items-center text-lg">{{ item.name }}</div>
+      </div>
+    </div>
+    <div class="w-full flex justify-between">
+      <div class="w-full mr-1">
+        <div class="btn w-full bg-blue-700 hover:bg-blue-800 border-0">
+          Login
+        </div>
+      </div>
+      <div class="w-full ml-1">
+        <div class="btn w-full border-0">Register</div>
       </div>
     </div>
   </div>
@@ -37,12 +48,12 @@ import { defineComponent, ref, useContext } from '@nuxtjs/composition-api';
 export default defineComponent({
   setup() {
     const items = [
-      { name: 'Dashboard', icon: 'home' },
-      { name: 'Prakit', icon: 'home' },
-      { name: 'Ruangrit', icon: 'home' },
-      { name: 'Sapondanai', icon: 'home' },
+      { name: 'Home', icon: 'home', path: '' },
+      { name: 'Prakit', icon: 'home', path: '' },
+      { name: 'Ruangrit', icon: 'home', path: '' },
+      { name: 'Sapondanai', icon: 'home', path: '' },
     ];
-    const selectedItem = ref('Dashboard');
+    const selectedItem = ref('Home');
 
     const selectItem = (item) => {
       selectedItem.value = item;
