bb17aff952c561f452d44f69c349429d714a3a9e
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-09-29 22:00:06 +0700

    [Update] RightSidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,10 @@
       <layout-LeftSidebar />
     </div>
     <nuxt class="lg:col-span-4 col-span-6" />
-    <div class="lg:flex border-l border-gray-400 flex flex-col">
+    <div class="lg:flex hidden border-l border-gray-400">
+      <layout-RightSidebar />
+    </div>
+    <!-- <div class="lg:flex border-l border-gray-400 flex flex-col">
       <p>Sidebar</p>
       <button v-if="$auth.user" class="btn btn-warning" @click="$auth.logout()">
         Logout
@@ -12,6 +15,6 @@
       <button v-else class="btn btn-secondary" @click="$router.push('/auth')">
         Login
       </button>
-    </div>
+    </div> -->
   </div>
 </template>
