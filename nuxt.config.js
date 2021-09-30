ee44794d2b0ca24c7b84a0efc8e3496ba6c145a6
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-09-30 23:03:22 +0700

    [Update] Credentials

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,7 +99,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     baseURL: process.env.API_BASE_URL,
-    credentials: true,
+    withCredentials: true,
     // proxy: true,
   },
   // proxy: {
