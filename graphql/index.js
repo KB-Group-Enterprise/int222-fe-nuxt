08e0f03f222769f8ab1c17b2153fec324ab4a8d6
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-11-28 14:37:58 +0700

    [Update] add apollo upload client, [Fixed] can't send image bug

--- graphql/index.js
+++ graphql/index.js
@@ -2,6 +2,7 @@ import { HttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { from } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createUploadLink } from 'apollo-upload-client';
 export default (ctx) => {
   const ssrMiddleware = setContext((_, { headers }) => {
     const token = localStorage.getItem('auth._token.local');
@@ -16,7 +17,12 @@ export default (ctx) => {
     uri: process.env.API_BASE_URL + '/graphql',
     withCredentials: true,
   });
-  const link = from([ssrMiddleware, httpLink]);
+  const uploadLink = createUploadLink({
+    uri: process.env.API_BASE_URL + '/graphql',
+    withCredentials: true,
+  });
+
+  const link = from([ssrMiddleware, uploadLink]);
   const cache = new InMemoryCache();
 
   return {
