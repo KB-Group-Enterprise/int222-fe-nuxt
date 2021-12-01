f5bf3e2f17ddcb3b829a38bea649a385f76a8f27
akira2542 <akiraakamura@gmail.com>
2021-12-01 18:50:32 +0700

    [Add] Error Page

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full mt-20 text-center justify-center">
+    <div class="p-5 rounded-xl border border-gray-500">
+      <h1 v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occurred</h1>
+      <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'error', // you can set a custom layout for the error page
+};
+</script>
